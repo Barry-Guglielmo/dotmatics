3b249da59f73b5eb8d15c264f435b3cc70ab4c21
root <root@exo-stage.onschrodinger.com>
2023-10-10 18:41:46 +0000

    api

--- api.py
+++ api.py
@@ -1,17 +1,41 @@
 from config import *
 from dotmatics_api_wrappers import *
 
-def basic_project_info(session, name_only=True):
-    if name_only == False:
-        d = {}
+
+class Basic_Project_Info():
+    def __init__(self, session, filter = False, config_projects=['LIVEDESIGN_REG_INT','LIVEDESIGN_STUDIES_INT']):
+        '''
+        This will make it easier to look through different projects information
+        list(.project_names)
+
+        .datasource_names['<project>'] # example: LIVEDESIGN_REG_INT, LIVEDESIGN_STUDIES_INT
+
+        if filter = True then only config_projects info will be displayed
+        '''
+
+        self.session = session
+        self.config_projects = config_projects
+
+        info = {}
+        names = {}
+        datasources = {}
+        datasource_names = {}
+
         for i in session.get_projects():
-            d[i] = s.get_project(i)
-        return d
-    else:
-        d = {}
-        for i in session.get_projects():
-            try:
-                d[session.get_project(i)['projectName']] = i
-            except:
-                print(i)
-        return d
+            pi = session.get_project(i)
+
+            if filter == True and pi['projectName'] in config_projects:
+                info[i] = pi
+                names[pi['projectName']] = i
+                datasources[pi['projectName']] = pi['dataSources']
+                datasource_names[pi['projectName']] = [[k['name'],k['dsID']] for j,k in datasources[pi['projectName']].items()]
+            elif filter ==False:
+                info[i] = pi
+                names[pi['projectName']] = i
+                datasources[pi['projectName']] = pi['dataSources']
+                datasource_names[pi['projectName']] = [[k['name'],k['dsID']] for j,k in datasources[pi['projectName']].items()]
+
+        self.project_info = info
+        self.project_names = names
+        self.datasources = datasources
+        self.datasource_names = datasource_names
--- dotmatics_api_wrappers.py
+++ dotmatics_api_wrappers.py
@@ -1,9 +1,15 @@
 import requests
 import json
+from aiohttp import BasicAuth
 
 # curl -H X-CDD-Token:$TOKEN 'https://app.collaborativedrug.com/api/v1/vaults/23/protocols?async=true'
 class Session:
-    def __init__(self,url, user = '', password = '', token = False):
+
+    def get(self, endpoint):
+        response = requests.get(url=endpoint)
+        return response
+
+    def __init__(self,url, user = '', password = '', token = False, expire = 86400):
         '''
         For authentication, you are required to provide valid isid (username) and password parameters, or a
         security token, with every request:
@@ -12,25 +18,23 @@ class Session:
         self.url = url
         self.user = user
         self.password = password
+        self.expire = expire
+        self._auth = BasicAuth(self.user, self.password)
+
         if token != False:
             self.creds = '?token=%s'%token
         else:
-            # PROBABLY SHOULD REQUEST A TOKEN HERE
-            # /authenticate/requestToken?expiration=<seconds>
-            self.creds = '?isid=%s&password=%s'%(user, password)
-
-    def get(self, endpoint):
-        response = requests.get(url=endpoint)
-        return json.loads(response.text)
-
+            self.plain_creds = '?isid=%s&password=%s'%(user, password)
+            self.token =  self.get(self.url+'/browser/api/authenticate/requestToken%s&expiration=%i'%(self.plain_creds,self.expire)).text.strip('"')
+            self.creds = '?token=%s'%self.token
     def get_collection(self, collection):
         '''/browser/api/<collection>'''
         return self.get(self.url+'/browser/api/%s%s'%(str(collection), self.creds))
-    
+
     def get_users(self):
         '''/browser/api/users'''
         return self.get(self.url+'/browser/api/users')
-    
+
     def get_datasources(self, project = 'all'):
         '''
         Get all datasources or all datasources from a project
@@ -38,32 +42,47 @@ class Session:
         '''
         if project == 'all':
             # returns list of ds [514,234,...]
-            return self.get(self.url+'/datasources%s'%(self.creds))
+            return self.get(self.url+'/browser/api/datasources%s'%(self.creds))
         else:
-            return self.get(self.url+'/datasources/project/%s%s'%(project, self.creds))
+            return self.get(self.url+'/browser/api/datasources/project/%s%s'%(project, self.creds))
 
     def get_datasource_data(self, project, data_source):
         '''
         /data/<isid>/<projectid>/<dskeys>/<ids>
-        
         A wildcard “*” may be specified to retrieve data for 
         all of the IDs from the project’s summary datasource.
         '''
 
-        return self.get(self.url+'/data/*/')
-    
+        return self.get(self.url+'/browser/api/data/*/')
+
     def get_structures(self, project):
         '''/structure/<isid>/<projectid>/<ids>'''
-        return self.get(self.url+'/structure/*/%s%s'%(project, self.creds))
+        return self.get(self.url+'/browser/api/structure/*/%s%s'%(project, self.creds))
 
     def get_projects(self):
-        '''/projects/'''
-        return self.get(self.url+'/projects/*/%s%s'%(project, self.creds))
+        '''/projects/
+
+           Returns project pks
+        '''
+        projs = self.get(self.url+'/browser/api/projects%s'%(self.creds)).text.strip('][').split(',')
+        return [int(i.strip('"')) for i in projs]
 
     def get_project(self, project):
-        '''/projects/<projectid>'''
-        return self.get(self.url+'/projects/*/%s%s'%(project, self.creds))
-    
+        '''/projects/<projectid>
+        Returns dictionary of project info
+
+        ['projectID', 'projectName', 'projectType', 'projectTypeDescr',
+         'projectCategory', 'description', 'defaultJoinColumn',
+         'JOIN_FORMAT', 'defaultDateColumn', 'primaryRegExp', 'primaryLength',
+         'dateFormatOracle', 'dateFormatJava', 'projectForms', 'dataSources',
+         'throttle', 'autoButtons', 'custom_img_size', 'isEditable', 'isHidden',
+         'dynamicStructures', 'orderby', 'latest_date', 'count', 'POPULATE_PROJECT_NAME_YN',
+         'AUDIT_YN', 'run_after_query', 'exclusive', 'numberFormats', 'excel_img_size',
+         'listBindings', 'autorun', 'productonly', 'BROWSEMODEEDITING', 'autofetchRestful']
+        '''
+        d = self.get(self.url+'/browser/api/projects/%i%s'%(project, self.creds)).text
+        return json.loads(d)
+
     def get_query(self, endpoint):
         '''/query/<isid>/<projectID>/<dsid>/<dskeys>/<operator>/<parameter>'''
         return 0
